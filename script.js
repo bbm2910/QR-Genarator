0ab3496c25f9bac1b037d07c8280f49e9213d5b5
Bobby M <b.mastacan@gmail.com>
2023-09-01 13:26:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const qrBox = document.querySelector(".qr-box");
 const qrImg = document.querySelector(".qr-img");
 const qrInput = document.querySelector(".qr-input");
+const paragraph = document.querySelector(".qr-box-p");
 const btn = document.querySelector(".btn");
 
 
@@ -19,3 +20,19 @@ function generateQrCode() {
 }
 
 btn.addEventListener("click", generateQrCode);
+
+function updateColor() {
+    const paragraph = document.querySelector(".qr-box-p");
+
+    // Toggle between the two color classes
+    if (paragraph.classList.contains("change-color")) {
+        paragraph.classList.remove("change-color");
+        paragraph.classList.add("change-color-2");
+    } else {
+        paragraph.classList.remove("change-color-2");
+        paragraph.classList.add("change-color");
+    }
+}
+
+const intervalId = setInterval(updateColor, 700);
+
